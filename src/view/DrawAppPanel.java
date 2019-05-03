28dc84e5eb66ebe025b745fd7c4e7336724ee849
Aaron Christiansen <125aaron@gmail.com>
2019-05-03 13:52:59 -0600

    got some buttons done

--- src/view/DrawAppPanel.java
+++ src/view/DrawAppPanel.java
@@ -5,12 +5,36 @@ import controller.ArtController;
 
 public class DrawAppPanel
 {
+	private SpringLayout appLayout;
+	private JButton redButton;
+	private JButton blueButton;
+	private JButton yellowButton;
+	private JButton orangeButton;
+	private JButton pinkButton;
+	private JButton blackButton;
+	private JButton whiteButton;
+	private JButton violetButton;
+	
 	private ArtController app;
 	
 	public DrawAppPanel(ArtController app)
 	{
 		super();
 		this.app = app;
+		redButton = new JButton("Red");
+		blueButton = new JButton("Blue");
+		yellowButton = new JButton("Yellow");
+		orangeButton = new JButton("Orange");
+		pinkButton = new JButton("Pink");
+		blackButton = new JButton("Black");
+		whiteButton = new JButton("White");
+		violetButton = new JButton("Violet");
+		
+		setupMenuPanels();
+		setupScrollPanel();
+		setupPanel();
+		setupLayout();
+		setupLiseners();
 	}
 	
 	private void setupMenuPanels()
@@ -23,11 +47,14 @@ public class DrawAppPanel
 		
 	}
 	
-	private void setupPanel()
+	public void setupPanel()
 	{
-		
+		this.setLayout(appLayout);
+		this.add(redButton);
+		this.add(redButton);
 	}
 	
+
 	private void setupLayout()
 	{
 		
