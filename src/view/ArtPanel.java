3ddd5290d34c4224e974cd5a50a7afe4073b32c0
Aaron Christiansen <125aaron@gmail.com>
2019-05-03 13:25:29 -0600

    added all the panels

--- src/view/ArtPanel.java
+++ src/view/ArtPanel.java
@@ -1,6 +1,15 @@
 package view;
 
+import javax.swing.JPanel;
+import controller.ArtController;
+
 public class ArtPanel
 {
-
+	private ArtController app;
+	
+	public ArtPanel(ArtController app)
+	{
+		super();
+		this.app = app;
+	}
 }
---
+++ src/view/DrawAppPanel.java
@@ -0,0 +1,40 @@
+package view;
+
+import javax.swing.*;
+import controller.ArtController;
+
+public class DrawAppPanel
+{
+	private ArtController app;
+	
+	public DrawAppPanel(ArtController app)
+	{
+		super();
+		this.app = app;
+	}
+	
+	private void setupMenuPanels()
+	{
+		
+	}
+	
+	private void setupScrollPanel()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupLiseners()
+	{
+		
+	}
+}
