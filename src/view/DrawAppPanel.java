dfa307f509c0282e98c699234526ce2c3944463b
Aaron Christiansen <125aaron@gmail.com>
2019-05-07 12:48:22 -0600

    added set up slider

--- src/view/DrawAppPanel.java
+++ src/view/DrawAppPanel.java
@@ -1,11 +1,20 @@
 package view;
 
+import java.awt.GridLayout;
+import java.util.Hashtable;
+
 import javax.swing.*;
 import controller.ArtController;
 
 public class DrawAppPanel
 {
+	
 	private SpringLayout appLayout;
+	private ArtPanel canvas;
+	private JPanel colorPanel;
+	private JPanel menuPanel;
+	private JScrollPane canvasPane;
+	
 	private JButton redButton;
 	private JButton blueButton;
 	private JButton yellowButton;
@@ -16,6 +25,9 @@ public class DrawAppPanel
 	private JButton violetButton;
 	
 	private ArtController app;
+	
+	private static final int MINIMUM_LINE = 1;
+	private static final int MAXIMUM_LINE = 25;
 	
 	public DrawAppPanel(ArtController app)
 	{
@@ -30,13 +42,37 @@ public class DrawAppPanel
 		whiteButton = new JButton("White");
 		violetButton = new JButton("Violet");
 		
+		saveButton = new JButton("save panel");
+		loadButton = new JButton("load panel");
+		clearButton = new JButton("clear panel");
+		widthSlider = new JSlider(MINIMUM_LINE, MAXIMUM_LINE);
+		colorPanel = new JPanel(new GridLayout(0,1));
+		menuPanel = new JPanel(new GridLayout(0,1));
+		
+		
 		setupMenuPanels();
 		setupScrollPanel();
+		setupSlider();
 		setupPanel();
 		setupLayout();
 		setupLiseners();
 	}
 	
+	private void setupSlider()
+	{
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>()
+		scaleLabels.put(MINIMUM_LINE, new JLabel("<HTML>Small<BR>Line</HTML>"));
+		scaleLabels.put(MINIMUM_LINE, new JLabel("<HTML>Large<BR>Line</HTML>"));
+		widthSlider.setLabelTabel(scaleLables);
+		widthSlider.setSnapToTicks(true);
+		widthSlider.setMajorTickSpacing(5);
+		widthSlider.setMinorTickSpacing(1);
+		widthSlider.setpaintTicks(true);
+		widthSlider.setPaintLabels(true);
+		widthSlider.set((MAXIMUM_LINE + MINIMUM_LINE) / 2);
+		
+	}
+
 	private void setupMenuPanels()
 	{
 		
@@ -44,14 +80,14 @@ public class DrawAppPanel
 	
 	private void setupScrollPanel()
 	{
-		
+
 	}
 	
 	public void setupPanel()
 	{
 		this.setLayout(appLayout);
 		this.add(redButton);
-		this.add(redButton);
+		this.add(blueButton);
 	}
 	
 
